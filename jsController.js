77536e57f34d3627cd280b5fdcd3c3ead3625eb3
esouthren <eilidhsouthren@gmail.com>
2017-03-05 13:09:25 +0000

    Eilidh's mobile menu test
    Added desktop/mobile fixes

--- jsController.js
+++ jsController.js
@@ -1,12 +1,27 @@
-var abEvents = angular.module('abEvents', []);
+var abEvents = angular.module("abEvents", ['ngAnimate']);
 
-abEvents.controller('abEventsController', function abEventsController($scope, $location) {
+abEvents.controller("abEventsController", function($scope) {
 	
-	$scope.mobileMenuToggle = true;
+    // toggles visibility of menu on mobile devices
+	$scope.mobileMenuToggle = false;
+    
+    
+	//a test array of events
+	$scope.testEvents = ['one', 'two', 'three', 'four', 'five', 'six', 'sev', 'eight'];
+    
+    // what
+	$scope.test = "What";
 	
-	$scope.toggleMenu = funtion() {
+    // function called when Menu button is pressed on mobile
+	$scope.toggleMenu = function() {
 		$scope.mobileMenuToggle = ($scope.mobileMenuToggle) ? false : true;
 	}
 	
 	
-}
+});
+
+
+// get window size for menu reshuffling
+
+    
+
