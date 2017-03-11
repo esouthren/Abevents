f15d4a02f3cefdfa0c81bdb10bc6c211a67dea52
esouthren <eilidhsouthren@gmail.com>
2017-03-11 16:06:52 +0000

    added files
    matched fonts, background colours

--- jsController.js
+++ jsController.js
@@ -6,6 +6,9 @@ abEvents.controller("abEventsController", function($scope) {
 	$scope.mobileMenuToggle = false;
 
 	$scope.eventExpandToggle = true;
+    
+    // event collapsing animation variable
+    $scope.isCollapsed = false;
 
 	$scope.lorum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sollicitudin sapien eu hendrerit tincidunt. Aliquam sit amet elit nibh. 	Aenean et lorem sed mauris pharetra fermentum. Pellentesque placerat magna vel imperdiet dapibus. Nam ac velit velit. Sed nibh quam, semper consectetur pretium vel, consequat ut dui. Proin in maximus erat.Nulla commodo pulvinar laoreet. Ut sed elit est. Morbi dui velit, tristique at mauris consequat, molestie maximus lectus. Maecenas ornare viverra vestibulum";
 
@@ -22,7 +25,7 @@ abEvents.controller("abEventsController", function($scope) {
 			image: "EventIcon2.jpg"
 		},
 		{
-			name: "Rare Thursday",
+			name: "Rare Thursdays",
 			location: "Tunnels",
 			date: "42nd Blocktober",
 			time: "7900 - 1202",
@@ -62,13 +65,50 @@ abEvents.controller("abEventsController", function($scope) {
 
 	$scope.expandEvent = function() {
 
-		$scope.eventExpandToggle = ($scope.eventExpandToggle) ? false : true;
+		$scope.eventExpandToggle = !$scope.eventExpandToggle;
+        $scope.isCollapsed = !$scope.isCollapsed;
 
 
 	}
 
 
 });
+
+// animating the event expansion
+
+abEvents.directive('collapse', [function () {
+		return {
+			restrict: 'A',
+
+			link: function ($scope, ngElement, attributes) {
+				var element = ngElement[0];
+
+				$scope.$watch(attributes.collapse, function (collapse) {
+					var newHeight = collapse ? 0 : getElementAutoHeight();
+                    console.log("new Height", newHeight);
+
+					element.style.height = newHeight + 'px';
+					ngElement.toggleClass('collapsed', collapse);
+				});
+
+				function getElementAutoHeight() {
+					var currentHeight = getElementCurrentHeight();
+
+					element.style.height = 'auto';
+					var autoHeight = getElementCurrentHeight();
+
+					element.style.height = currentHeight;
+					getElementCurrentHeight(); // Force the browser to recalc height after moving it back to normal
+
+					return autoHeight;
+				}
+
+				function getElementCurrentHeight() {
+					return element.offsetHeight
+				}
+			}
+		};
+	}]);
 
 
 // get window size for menu reshuffling
