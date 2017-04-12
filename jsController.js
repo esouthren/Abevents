3558c3df1454a13e35de126e5b618abee947441e
esouthren <eilidhsouthren@gmail.com>
2017-04-12 22:15:16 +0100

    Add files via upload
    angular API updates and prettification

--- jsController.js
+++ jsController.js
@@ -1,156 +1,117 @@
+// Angular Controller by ES
+// Abevents Web Application, April 2017
 
 
+// define application
 var abEvents = angular.module("abEvents", ['ngMap', 'ngAnimate']);
 
-
-
-abEvents.controller("abEventsController", function($scope, $http) {
+//define controller for application 
+abEvents.controller("abEventsController", function($scope, $http, $timeout) {
     
-    
-    $scope.test = "hey dere";
-    
-    $scope.response = "API response!";
-    
+    // variables to declare at the start
+    $scope.priceMessage = "Wubba Lubba";
+    $scope.test = "hey dere";    
+    $scope.response = "API response!";    
+	$scope.eventExpandToggle = true;
+   
     
     // API call 
-
-    
     $scope.apiCall = function() {
         
-       console.log("hmm");
+        // empty data to clear the screen
+        $scope.response = null; 
+        // turn on the loading gif
+        $scope.loading = true;
+        // set API key 
+        $http.defaults.headers.common["Ocp-Apim-Subscription-Key"] = '7f3028ae78924451854a93a4151e2733';
+        // aight here we go this is the good stuff right here
         $http({
         
-        url: 'https://www.eventbriteapi.com/v3/events/search/', 
-        
+            url: 'https://api.allevents.in/events/geo/', 
+    
         params: {
             
             // this is where the user input is submitted
-            "token": "G7VABS45YU62M37KJ2I4",
-            "location.address": "Aberdeen, UK",
-            "location.within": "20mi", 
-            "categories": "", // categories here
-            "price": "", //insert "free" or "paid" (how to do both?)
-            "start_date.range_start": "2017-04-05T08:00:00", // format: "YYYY-MM-DDTHHMMSS"
-            "start_date.range_end": "2017-10-05T23:59:59",
-            "start_date.keyword": "", // caninclude "this_week", "next_week", "this_weekend", "tomorrow", "today"
+            "latitude": 57.146114,
+            "longitude": -2.091476,
+            "radius": 15,
+            "category": "", // keywords be going here
             
-            
+            // these are the only search parameters for the API. So, further refining (like dates)
+            // will need to be applied to the data returned below.
         },
-        method: "GET",
+        method: "POST",
        
-      }).success(function(response){
-       
-        console.log(response);
-            $scope.response = response.events;
-            console.log(response.events[1]);
-       
+            // if successful API call: 
+          }).success(function(response){
+            // hide the loading gif    
+           $scope.loading = false;
+
+            
+        // log the data to the console for inspection (right click > inspect > console to view)    
+        console.log(response);        
+        console.log(response.data[1]);
+            
+        // let's format our data to make it more friendly looking    
+            
+        // apply a pricing message to each event (checking if the event has tickets and
+        // changing the message accordingly)
+            
+        // loop through the data array and check it's Ticketing status    
+        for (var i = 0; i < response.data.length; i++) {
+            
+            response.data[i].priceMessage = "";
+            if (response.data[i].tickets.has_tickets == true) {
+                response.data[i].priceMessage = "Get tickets at: " + response.data[i].tickets.ticket_url;
+            }
+            else {
+                response.data[i].priceMessage = "This event is free!";                
+            }
+        }
+            
+        // NOTE ********* more things we need to do to the data:
+            // get the icons displaying - I'm thinking have them all there in the HTML but displaying based on a boolean
+            // value we set here? Like if tickets.has_tickets == false.
+        
+        // finally, bind the data to the $scope.response variable to be used in the View bit (the HTML).    
+        $scope.response = response.data;        
         
       });
+        
+        // after api call, close menu (if we're in mobile mode)
+        if (window.innerWidth < 750) {
+	       $scope.mobileMenuToggle = true;
+        }
     }
 
-	$scope.directions = [
-          {center: [57.133680,-2.227462]},
-            {center: [57.133680,-2.227462]},
-          {center: [57.133680,-2.227462]},
-
-        ];
-
     // toggles visibility of menu on mobile devices
-		$scope.mobileMenuToggle = false;
-	if (window.innerWidth < 750) {
-	$scope.mobileMenuToggle = true;
-}
+    $scope.mobileMenuToggle = false;
+	   if (window.innerWidth < 750) {
+	       $scope.mobileMenuToggle = true;
+        }
 
 
-	$scope.eventExpandToggle = true;
-
-    // event collapsing animation variable
-    $scope.isCollapsed = false;
-
-	$scope.lorum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sollicitudin sapien eu hendrerit tincidunt. Aliquam sit amet elit nibh. 	Aenean et lorem sed mauris pharetra fermentum. Pellentesque placerat magna vel imperdiet dapibus. Nam ac velit velit. Sed nibh quam, semper consectetur pretium vel, consequat ut dui. Proin in maximus erat.Nulla commodo pulvinar laoreet. Ut sed elit est. Morbi dui velit, tristique at mauris consequat, molestie maximus lectus. Maecenas ornare viverra vestibulum";
-
-
-	//a test array of events
-	$scope.testEvents = [
-		{
-			name: "Crash Wednesdays",
-			location: "Underground",
-			date: "3rd March",
-			time: "2300 - 0300",
-			description: $scope.lorum,
-			price: 5,
-			image: "EventIcon2.jpg",
-			iconAge: "iconParents.png",
-			iconCost: "iconFree.png",
-			iconParking: "iconParking.png",
-			categoryImg: "Theatre.png"
-		},
-		{
-			name: "Rare Thursdays",
-			location: "Tunnels",
-			date: "42nd Blocktober",
-			time: "7900 - 1202",
-			description: $scope.lorum,
-			price: 0,
-			image: "EventIcon.jpg",
-
-			iconAge: "icon18.png",
-			iconCost: "iconPay.png",
-			iconParking: "iconParking.png",
-			categoryImg: "NightClub.png"
-
-		},
-		{
-			name: "Crash Wednesdays",
-			location: "Underground",
-			date: "3rd March",
-			time: "2300 - 0300",
-			description: $scope.lorum,
-			price: 5,
-			image: "EventIcon2.jpg",
-
-			iconAge: "icon18.png",
-			iconCost: "iconPay.png",
-			iconParking: "iconParking.png"
-		},
-		{
-			name: "Crash Wednesdays",
-			location: "Underground",
-			date: "3rd March",
-			time: "2300 - 0300",
-			description: $scope.lorum,
-			price: 5,
-			image: "EventIcon2.jpg",
-
-			iconAge: "icon18.png",
-			iconCost: "iconPay.png",
-			iconParking: "iconParking.png"
-		}
-	];
-
-    
 
     // what
 	$scope.test = "What";
 
-    // function called when Menu button is pressed on mobile
+    // function called when Menu button is pressed on mobile - expands/minimises the menu
 	$scope.toggleMenu = function() {
 		$scope.mobileMenuToggle = ($scope.mobileMenuToggle) ? false : true;
-	}
-    
+	}    
     
     // expand the event to display more descriptive text
 	$scope.expandEvent = function() {
 
 		$scope.eventExpandToggle = !$scope.eventExpandToggle;
-        $scope.isCollapsed = !$scope.isCollapsed;
+        $scope.isCollapsed = !$scope.isCollapsed;  
 
-
-	}
-
-
+	} 
+    
 });
 
+
+// ****** tiiiiiiiiny buggies, in the code, make me happy, make me feel fine ******* 
 
 // function for formatting the event description text properly (as the returned string contains HTML)
 
